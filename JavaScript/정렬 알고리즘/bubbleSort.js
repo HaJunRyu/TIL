ff6403820dbcd73b2c25bb55b41756eaf699bab2
HaJunRyu <hjr4557@naver.com>
2020-12-07 22:36:00 +0900

    alignment algolizm

---
+++ JavaScript/정렬 알고리즘/bubbleSort.js
@@ -0,0 +1,16 @@
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > array[i + 1]) {
+      const changeNum = array[i + 1];
+      array[i + 1] = array[i];
+      array[i] = changeNum;
+      i = -1;
+    }
+  }
+  
+  return array;
+}
+
+console.log(bubbleSort([2, 4, 5, 1, 3])); // [1, 2, 3, 4, 5]
+console.log(bubbleSort([5, 2, 1, 3, 4, 6])); // [1, 2, 3, 4, 5, 6]
+console.log(bubbleSort([3, 1, 0, -1, 4, 2])); // [-1, 0, 1, 2, 3, 4]
