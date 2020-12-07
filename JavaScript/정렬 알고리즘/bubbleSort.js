7107dfa8c28661deb79f078149d0a4c195ebb888
HaJunRyu <hjr4557@naver.com>
2020-12-08 00:21:21 +0900

    bubbleSort

--- JavaScript/정렬 알고리즘/bubbleSort.js
+++ JavaScript/정렬 알고리즘/bubbleSort.js
@@ -1,5 +1,5 @@
 function bubbleSort(array) {
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length - 1; i++) {
     if (array[i] > array[i + 1]) {
       const changeNum = array[i + 1];
       array[i + 1] = array[i];
